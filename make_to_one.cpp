925b2afc2d8ae926d22ff2511d2e691ccf753988
hando95 <sllover95@gmail.com>
2021-07-21 16:41:03 +0900

    dynamic programming1

---
+++ make_to_one.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int oper_cnt[1000001];
+
+int GetMin(int a, int b) {
+	return a < b ? a : b;
+}
+
+int GetOperMinCnt(int n) {
+	int i;
+	int temp1 = 0, temp2 = 0, temp3 = 0;
+	int flag_mul3 = 0, flag_mul2 = 0;
+	
+	oper_cnt[1] = 0;
+	
+	if (n == 1)
+		return 0;
+
+	for (i = 2; i <= n; i++) {
+
+		if (i % 3 == 0 && i % 2 == 0) {
+			oper_cnt[i] = GetMin(GetMin(oper_cnt[i / 3] + 1, oper_cnt[i / 2] + 1), oper_cnt[i - 1] + 1);
+		}
+		else if (i % 3 == 0 && i % 2 != 0) {
+			oper_cnt[i] = GetMin(oper_cnt[i / 3] + 1, oper_cnt[i - 1] + 1);
+		}
+		else if (i % 3 != 0 && i % 2 == 0) {
+			oper_cnt[i] = GetMin(oper_cnt[i / 2] + 1, oper_cnt[i - 1] + 1);
+		}
+		else
+			oper_cnt[i] = oper_cnt[i - 1] + 1;
+
+	}
+	return oper_cnt[n];
+}
+
+
+int main() {
+	int n;
+
+	scanf("%d", &n);
+	printf("%d\n", GetOperMinCnt(n));
+	return 0;
+}
