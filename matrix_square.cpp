f8a6a2ca6f2851a95424add90d825def4a742692
CodeHan-95 <sllover95@gmail.com>
2021-09-06 12:47:28 +0900

    divide & conquer

--- matrix_square.cpp
+++ matrix_square.cpp
@@ -1,67 +1,71 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_MAT 5
+#include <stdio.h>
 
-long long N, B;
-long long A[5][5];
-long long temp[5][5];
-long long ans[5][5];
+long long mat[MAX_MAT][MAX_MAT];
+long long ans[MAX_MAT][MAX_MAT];
+long long temp[MAX_MAT][MAX_MAT];
 
-
-// 배열 출력
-void print_arr(long long arr[5][5])
-{
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-			cout << arr[i][j] << " ";
-		cout << endl;
-	}
-}
-
-// 행렬 곱셈
-void Matrix_multi(long long X[5][5], long long Y[5][5])
-{
-
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-		{
-			temp[i][j] = 0; // 행렬 초기화
-			for (int k = 0; k < N; k++)
-				temp[i][j] += (X[i][k] * Y[k][j]);
-
+/* 정답행렬에 A행렬을 곱하는 함수 */
+void Production(long long n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			temp[i][j] = 0;
+			for (int k = 0; k < n; k++) {
+				temp[i][j] += (ans[i][k] * mat[k][j]) % 1000;
+			}
 			temp[i][j] %= 1000;
 		}
-
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			X[i][j] = temp[i][j];
-}
-
-int main()
-{
-	cin >> N >> B;
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-			cin >> A[i][j];
-		ans[i][i] = 1; // 정답행렬은 단위행렬로
 	}
-
-	while (B > 0)
-	{
-		if (B % 2 == 1)
-		{
-			Matrix_multi(ans, A); // 정답행렬에 A행렬 곱하기
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			ans[i][j] = temp[i][j];
 		}
-		Matrix_multi(A, A);
-		B /= 2;
 	}
-
-	print_arr(ans); // A를 B번 곱한 결과
-
 }
 
-// 출처: https://cocoon1787.tistory.com/293
+/* A행렬을 제곱하는 함수 */
+void Square(long long n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			temp[i][j] = 0;
+			for (int k = 0; k < n; k++) {
+				temp[i][j] += (mat[i][k] * mat[k][j]) % 1000;
+			}
+			temp[i][j] %= 1000;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			mat[i][j] = temp[i][j];
+		}
+	}
+}
+
+int main() {
+	long long n;
+	long long b;
+	scanf("%lld %lld", &n, &b);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%lld", &mat[i][j]);
+		}
+		ans[i][i] = 1;
+	}
+
+	while (b) {
+		if (b % 2 == 1)
+			Production(n);
+		Square(n);
+		b /= 2;
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%lld ", ans[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+// reference: https://cocoon1787.tistory.com/293
